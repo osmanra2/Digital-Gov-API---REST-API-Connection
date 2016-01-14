6798f76f6bd9a03cbf2e1c29d363c02c6557d7a3
Rene Osman <osmanra2@users.noreply.github.com>
2016-01-14 16:47:32 -0500

    api-test.php
    Very simple front end that connects to DG Rest API and returns results for you

---
+++ api-test.php
@@ -0,0 +1,70 @@
+<?php
+$end="https://search.usa.gov/api/v2/search";
+
+//insert your DG access key
+$key="";
+
+//insery your DG affiliate
+$aff="";
+
+
+if(!empty($_GET['query'])){
+
+$api_url = 'https://search.usa.gov/api/v2/search?affiliate='. $aff .'&access_key='. $key .'&query='. urlencode($_GET['query']);
+
+$search_json = file_get_contents($api_url);
+$search_array = json_decode($search_json, true);
+
+}
+
+
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Search API Test</title>
+	</head>
+	
+
+		<body>
+			<h1>Search API Test Results for Grants</h1>
+
+		<form action="" method="get" autocomplete="off">
+		<input type="text" name="query" class="form-control" placeholder="Search Twitter Database">
+		<br />
+		<button type="submit">Submit</button>
+		</form>
+
+
+		<?php
+		if($search_array['web']['total']>=1) {
+
+		echo "<p>Total Results " . $search_array['web']['total'] . "</p>";
+
+		}
+
+		else {
+
+			echo "<p>No Results for " . $_GET['query'] . "</p>";
+		}
+
+		foreach ($search_array['web']['results'] as $key) {
+		
+			echo '<p><h3>' . $key['title']. '</h3>';
+			echo '<a href="' . $key['url'] . '">'. $key['url'] .'</a>' ;
+			echo '<br /><strong>' . $key['snippet']. '</strong>';
+			echo '<br />' . $key['publication_date'] . '</p>';
+
+		}
+
+		?>
+
+
+		</body>
+
+
+
+
+</html>  
