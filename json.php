6ff551737a8a4382f60c16068b5732a5821f2759
Rene Osman <osmanra2@users.noreply.github.com>
2016-01-14 17:22:47 -0500

    updated to remove errors
    replaced !empty with isset

--- json.php
+++ json.php
@@ -7,19 +7,48 @@ $aff="";
 
 
 
-if(!empty($_GET['query'])){
+if(isset($_GET['query'])){
 
 $api_url = 'https://search.usa.gov/api/v2/search?affiliate='. $aff .'&access_key='. $key .'&query='. urlencode($_GET['query']);
 
 $search_json = file_get_contents($api_url);
 $search_array = json_decode($search_json, true);
 
-echo "<pre>";
-print_r($search_array);
-echo "</pre>";
-
 }
 
-
-
 ?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Search API Test</title>
+	</head>
+	
+
+		<body>
+			<h1>Search API Test Results for Grants</h1>
+
+		<form action="" method="get" autocomplete="off">
+		<input type="text" name="query" class="form-control" placeholder="Search">
+		<br />
+		<button type="submit">Submit</button>
+		</form>
+
+
+		<?php
+		if(isset($search_array)){ 
+
+		echo "<pre>";
+		print_r($search_array);
+		echo "</pre>";
+
+		}
+
+		?>
+
+
+		</body>
+
+
+
+
+</html>  
